73a051e727a8a5ea8e7cfbb57c02ea0a5c465f91
Dmitry Obrazok <d.obrazok@gmail.com>
2024-05-18 19:49:20 +0300

    yoyoyo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, Git!!! Yo Yo Leave this state brbrbrrb" << std::endl;
+    std::cout << "Hello, Git!!! Yo Yo Leave this state brbrbrrb aaaaaiiiooo" << std::endl;
 }
